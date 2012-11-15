798d08e5dabcd2f9ca235ac8d0902d1508e78612
Savant Krishna <savant.2020@gmail.com>
2012-11-15 17:12:59 +0530

    readme and build script

--- build_depend_doc.sh
+++ build_depend_doc.sh
@@ -1,6 +1,8 @@
 sudo apt-get install python-ply
 sudo apt-get install doxygen
-export PYTHONPATH=$PYTHONPATH:$path/epsilon
+export PYTHONPATH=$PYTHONPATH:$PWD/epsilon
 doxygen Doxyfile
 cd docs/
 pdflatex report.tex
+cd latex/
+make
